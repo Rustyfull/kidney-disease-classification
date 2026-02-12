bc373c4a0a1324c9752ac7f9d8cdb4812464e457
Rustyfull <kanarusty2002@gmail.com>
2026-02-12 22:08:05 +0100

    folder structure added

---
+++ template.py
@@ -0,0 +1,51 @@
+import os
+from errno import EALREADY
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s]: %(message)s:"
+)
+
+project_name = 'cnnClassifier'
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yml",
+    "dvc.yml",
+    "params.yml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "test.py"
+]
+
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    file_dir = file_path.parent
+
+    if file_dir != "":
+        os.makedirs(file_dir,exist_ok=True)
+        logging.info(f"Creating directory: {file_dir}")
+
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path,"w") as file_obj:
+            pass
+        logging.info(f"Creating empty file: {file_path}")
+
+    else:
+        logging.info(f"{file_path.name} already exists")
+
+
